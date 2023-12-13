a964dbb7f6ac8ed64a5b5f62bdbc47ec5f904d7f
walterbaya <walterbaya@yahoo.com>
2023-12-13 00:29:22 -0300

    se agrega contador de pagina actual

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,24 +12,24 @@ import "./Home.css";
 import CardContainer from "../components/CardContainer";
 
 const Home: React.FC = () => {
-  const slidesRef = useRef<HTMLIonSlidesElement>(null);
-  const [pokemones, setPokemones] = useState({});
+  var slidesRef = useRef<HTMLIonSlidesElement>(null);
+  var [page, setPage]= useState(1);
+  var [pokemones, setPokemones] = useState({});
 
   useEffect(() => {
-    fetchPokemons().then(response => console.log(response))
+    fetchPokemons(page).then(response => console.log(response))
     //setPokemones();
     console.log(pokemones)
   }, [])
 
-  async function fetchPokemons() {
-    const result = await fetch("https://beta.pokeapi.co/graphql/v1beta?limit=4&offset=4", {
+  async function fetchPokemons(i: number) {
+    const result = await fetch("https://beta.pokeapi.co/graphql/v1beta", {
       method: "POST",
       body: JSON.stringify({
-        query: `query getItems{pokemon_v2_pokemon(limit: 4, offset: 4) 
-          {height, base_experience, name, pokemon_v2_pokemonabilities {pokemon_v2_ability {name}}, weight}
+        query: `query getItems{pokemon_v2_pokemon(limit: 4, offset:`  + i*4 + 
+        `){height, base_experience, name, pokemon_v2_pokemonabilities {pokemon_v2_ability {name}}, weight}
         }
           `
-
         , variables: null,
         operationName: "getItems",
       }),
@@ -42,12 +42,16 @@ const Home: React.FC = () => {
     keyboard: true,
   };
 
-  const slideWillChange = () => {
-    console.log("Slide will change");
+  const ionSlideNextStart = () => {
+    fetchPokemons(page + 1).then(response => console.log(response))
+    //setPokemones();
+    console.log(pokemones)
+    console.log("NEXT");
+    setPage(page + 1);
   };
 
-  const slideDidChange = async () => {
-    console.log("Slide did change");
+  const ionSlidePrevStart = async () => {
+    console.log("PREV");
 
     if (!slidesRef.current) return;
 
@@ -75,8 +79,8 @@ const Home: React.FC = () => {
           pager={true}
           scrollbar={true}
           options={options}
-          onIonSlideWillChange={() => slideWillChange()}
-          onIonSlideDidChange={() => slideDidChange()}
+          onIonSlidePrevStart={() => ionSlidePrevStart()}
+          onIonSlideNextStart={() => ionSlideNextStart()}
           ref={slidesRef}
         >
           <IonSlide>
